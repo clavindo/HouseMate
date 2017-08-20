506fc1df486d9ba5478454776f4c92b72cd3b934
clavindo <nd.o@hotmail.com>
2017-08-20 22:48:24 +0900

    Main page, header in progress

--- index.php
+++ index.php
@@ -8,12 +8,43 @@
 	
 	<style>
 		body {
-			font-family: Arial, sans-serif;
+			font-family: Nanum-Gothic, sans-serif;
+			margin: 0;
+		}
+		
+		header {
+			border-bottom: 3px solid silver;
+		}
+		
+		ul {
+			float: right;
 		}
 		li {
-			float: right;
-			list-style-type: none;
+			list-style-type: none; /* Removes bullet points */
 			display: inline;
+			margin: 10px;
+			float: left;
+		}
+		li:hover {
+			border-bottom: 2px solid black;
+		}
+		li a {
+			text-decoration: none;
+			color: black;
+		}
+		div#content-menu {
+			clear: both;
+			margin-bottom: 5px;
+			margin-left: 5px;
+		}
+		div#content-menu a {
+			padding: 0 10px;
+			text-decoration: none;
+			color: #484848;
+		}
+		div#content-menu a:hover {
+			color: #008489;
+			border-bottom: 3px solid #008489;
 		}
 	</style>
 </head>
@@ -22,12 +53,25 @@
 
 <header>
 	
-	<ul>
-		<li><a href="#">호스팅 하기</a></li>
-		<li><a href="#">도움말</a></li>
-		<li><a href="#">회원가입</a></li>
-		<li><a href="#">로그인</a></li>
-	<ul>
+	<div>
+		<a href="/Project/HouseMate/index.php" >
+			<img src="pic/logo-ex.jpg" width="15%" height="15%" style="left: -50px"/>
+		</a>
+	
+		<ul>
+			<li><a href="#">호스팅 하기</a></li>
+			<li><a href="#">도움말</a></li>
+			<li><a href="#">회원가입</a></li>
+			<li><a href="#">로그인</a></li>
+		<ul>
+	</div>
+	
+	<div id="content-menu">
+		<a href="#">추천</a>
+		<a href="#">숙소</a>
+		<a href="#">트립</a>
+		<a href="#">장소</a>
+	</div>
 	
 </header>
 
