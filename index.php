e3423bf5ac929adc412fb3634ac0782415c7ddd7
rocket12213 <31176939+rocket12213@users.noreply.github.com>
2017-08-20 17:03:29 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
 <header>
 	
 	<ul>
-		<li><a href="#">호스팅 하기</a></li>
-		<li><a href="#">도움말</a></li>
-		<li><a href="#">회원가입</a></li>
-		<li><a href="#">로그인</a></li>
+		<li><a href="#">1. 호스팅 하기</a></li>
+		<li><a href="#">2. 도움말</a></li>
+		<li><a href="#">3. 회원가입</a></li>
+		<li><a href="#">4. 로그인</a></li>
 	<ul>
 	
 </header>
