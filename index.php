b5ccc857d3b217c3f460b7b19e8dabd709b31244
clavindo <nd.o@hotmail.com>
2017-08-20 16:16:53 +0900

    Initial Push

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>House Mate</title>
+</head>
+
+<body>
+
+<header>
+	
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	<ul>
+	
+</header>
+
+
+
+
+</body>
+
+</html>
